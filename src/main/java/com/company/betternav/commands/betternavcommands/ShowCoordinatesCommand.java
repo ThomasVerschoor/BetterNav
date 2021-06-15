e9f9092846e25b2031c844b45a3a60d6a1c657b4
thomasverschoor <thomas.verschoor29@gmail.com>
2021-06-15 17:48:34 +0200

    implemented showcoordinates commands

--- src/main/java/com/company/betternav/commands/betternavcommands/ShowCoordinatesCommand.java
+++ src/main/java/com/company/betternav/commands/betternavcommands/ShowCoordinatesCommand.java
@@ -8,39 +8,43 @@ import org.bukkit.entity.Player;
 
 import java.util.Map;
 
-public class ShowCoordinatesCommand extends BetterNavCommand {
+public class ShowCoordinatesCommand extends BetterNavCommand
+{
 
     private final FileHandler fileHandler;
-
     public ShowCoordinatesCommand(FileHandler fileHandler)
     {
         this.fileHandler = fileHandler;
     }
 
     @Override
-    public boolean execute(Player player, Command cmd, String s, String[] args, Map<String,String> messages) {
+    public boolean execute(Player player, Command cmd, String s, String[] args, Map<String,String> messages)
+    {
 
         // if location provided
-        if (args.length == 1) {
-            try {
-
+        if (args.length == 1)
+        {
+            try
+            {
                 // the location needed
                 String location = args[0];
 
                 //read coordinates out of file
                 LocationWorld coordinates = fileHandler.readFile(location,player);
 
-                if(coordinates==null){
-                    player.sendMessage("/bn to get information about how to use bn commands");
+                if(coordinates==null)
+                {
+                    player.sendMessage( messages.getOrDefault("error", "/bn to get information about how to use Betternav commands"));
                     return true;
                 }
 
                 //send coordinates to the player
-                player.sendMessage(coordinates.getName()+ " has coordinates X: "+String.valueOf(coordinates.getX())+ " Y:"+String.valueOf(coordinates.getY())+" and Z: "+String.valueOf(coordinates.getZ()));
+                player.sendMessage( messages.getOrDefault("has_coordinates"+" X:"+String.valueOf(coordinates.getX())+ " Y:"+String.valueOf(coordinates.getY())+" Z: "+String.valueOf(coordinates.getZ()), coordinates.getName()+ " has coordinates X: "+String.valueOf(coordinates.getX())+ " Y:"+String.valueOf(coordinates.getY())+" Z: "+String.valueOf(coordinates.getZ())));
 
-
-            } catch (IllegalArgumentException e) {
-                player.sendMessage("§c§l(!) §cThat is not a valid entity!");
+            }
+            catch (IllegalArgumentException e)
+            {
+                player.sendMessage( messages.getOrDefault("error", "/bn to get information about how to use Betternav commands"));
             }
         }
         return true;
